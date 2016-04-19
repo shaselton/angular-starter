98135baf9e203cda8a637ef1925a5b7b1e87610d
Scott Haselton <shaselton@gmail.com>
2016-04-18 21:28:22 -0700

    more structure and default modules

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,24 @@
 
 var gulp = require('gulp');
 
-gulp.task('default', function(){
+// Load plugins
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    cssnano = require('gulp-cssnano'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    concat = require('gulp-concat'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload');
 
+// Styles
+gulp.task('styles', function() {
+  return sass('src/styles/main.scss', { style: 'expanded' })
+    .pipe(autoprefixer('last 2 version'))
+    .pipe(gulp.dest('dist/styles'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(cssnano())
+    .pipe(gulp.dest('dist/styles'))
+    .pipe(notify({ message: 'Styles task complete' }));
 });
